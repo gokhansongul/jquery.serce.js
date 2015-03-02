10069cf226c4ea2889cfccbd0fc31c12150b045f
gokhansongul <gokhan.songul@n11.com>
2015-03-02 15:49:53 +0200

    initial commit.

---
+++ README.md
@@ -0,0 +1,46 @@
+Serce Slider Plugin for jQuery
+==============================
+Overview
+--------
+This is a serce slider plugin for the jQuery javascript library. It allows user to slide images on image mouseover.
+
+### Usage
+First, include the jQuery and serce javascript files.
+
+```html
+<script src="jquery.js" type="text/javascript"></script>
+<script src="jquery.serce.js" type="text/javascript"></script>
+```
+
+Next, add data-images attribute to img elements.
+
+```html
+    <div class="box">
+        <img class="serce" src="http://lorempixel.com/output/animals-h-c-270-320-8.jpg" data-images="http://lorempixel.com/output/animals-h-c-270-320-8.jpg, http://lorempixel.com/output/city-h-c-270-320-9.jpg, http://lorempixel.com/output/technics-h-c-270-320-6.jpg, http://lorempixel.com/output/technics-h-c-270-320-9.jpg" alt="serce dummy image"/>
+    </div>
+
+    <div class="box">
+        <img class="serce" src="http://lorempixel.com/output/animals-h-c-270-320-8.jpg" data-images="http://lorempixel.com/output/animals-h-c-270-320-8.jpg, http://lorempixel.com/output/city-h-c-270-320-9.jpg, http://lorempixel.com/output/technics-h-c-270-320-6.jpg" alt="serce dummy image"/>
+    </div>
+
+    <div class="box">
+        <img class="serce" src="http://lorempixel.com/output/animals-h-c-270-320-8.jpg" data-images="http://lorempixel.com/output/animals-h-c-270-320-8.jpg, http://lorempixel.com/output/city-h-c-270-320-9.jpg" alt="serce dummy image"/>
+    </div>
+```
+
+Next, call the serce function for those items you wish to have slided.
+
+```html
+
+jQuery(function($){
+   $(".box").serce();
+});
+```
+
+Optionally, you can pass the image selector.
+
+```html
+jQuery(function($){
+   $(".box").serce({imgSelector: 'serce'});
+});
+```
---
+++ jquery.serce.js
@@ -0,0 +1,79 @@
+(function( $ ) {
+
+    $.fn.serce = function(cfg) {
+
+        cfg = cfg || {};
+
+        var mode,
+            index,
+            length,
+            limits,
+            offsetX,
+            images,
+            imgSelector = cfg.imgSelector || 'img';
+
+        var mouseMove = function(e){
+
+            offsetX = e.offsetX;
+
+            for(var i = 0, ii = limits.length; i < ii; i++){
+                if(offsetX <= limits[i]) {
+                    index = i;
+                    break;
+                }
+                else if(offsetX > limits[i] && offsetX <= limits[i+1]){
+                    index = i + 1;
+                    break;
+                }
+            }
+
+            var $this = $(this);
+
+            $this.attr('src', images[index]);
+            var $serceWrapper = $this.parent().find(".serce-wrapper");
+            if ($serceWrapper.length) {
+                var $serceNavItem = $serceWrapper.find(".serce-nav-item");
+                $serceNavItem.removeClass("on").eq(index).addClass("on");
+            }
+
+        };
+        var mouseLeave = function(e){
+            $(this).find('.serce-wrapper').remove();
+        };
+
+        this.each(function() {
+            var limit,
+                $this = $(this);
+
+            $this.hover(function(){
+                var $img = $this.find(imgSelector);
+                images = $img.data("images") ? $img.data("images").split(",") : [];
+                length = images.length;
+                if(length > 1){
+                    mode = $img.width() / length;
+                    var thumbsMarkup = '';
+                    var serceNavItem = '<a class="serce-nav-item"></a>';
+                    var $serceWrapper = $('<div class="serce-wrapper"></div>');
+
+                    limits = [];
+                    for(var i = 1; i <= length; i++){
+                        limit = mode * i;
+                        limits.push(limit);
+                        thumbsMarkup += serceNavItem;
+                    }
+
+                    $this.find(".serce-wrapper").remove();
+                    $serceWrapper.insertAfter($img);
+                    $(thumbsMarkup).appendTo($serceWrapper);
+                    $serceWrapper.find(".serce-nav-item").first().addClass("on");
+                    $img.bind("mousemove", mouseMove);
+                }
+            }, mouseLeave);
+
+        });
+
+        return this;
+
+    };
+
+}( jQuery ));
