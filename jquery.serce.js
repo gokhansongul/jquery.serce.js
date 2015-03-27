3a0663019da7511262e2e19bbd72c5b95b929ae0
gokhansongul <gokhan.songul@n11.com>
2015-03-27 14:48:12 +0200

    Fixed firefox event problems and optimized mousemove event handling.

--- jquery.serce.js
+++ jquery.serce.js
@@ -10,26 +10,43 @@
             limits,
             offsetX,
             images,
-            imgSelector = cfg.imgSelector || 'img';
+            imgSelector = cfg.imgSelector || 'img',
+            noImagePath = cfg.noImagePath || "",
+            $wrapEl = $(this);
 
         var mouseMove = function(e){
 
-            offsetX = e.offsetX;
+            offsetX = e.offsetX ? e.offsetX : e.pageX - $(this).offset().left;
 
             for(var i = 0, ii = limits.length; i < ii; i++){
                 if(offsetX <= limits[i]) {
+                    if(index == i){
+                        return;
+                    }
                     index = i;
                     break;
                 }
                 else if(offsetX > limits[i] && offsetX <= limits[i+1]){
+                    if(index == i + 1){
+                        return;
+                    }
                     index = i + 1;
                     break;
                 }
             }
-
             var $this = $(this);
+            var newImg = new Image();
 
-            $this.attr('src', images[index]);
+            var loadFn = function(){ $this.attr('src', images[index]);};
+
+            newImg.onerror= function(){};
+
+            newImg.onload = loadFn;
+            newImg.src = images[index];
+            if(newImg.complete){
+                newImg.complete = loadFn;
+            }
+
             var $serceWrapper = $this.parent().find(".serce-wrapper");
             if ($serceWrapper.length) {
                 var $serceNavItem = $serceWrapper.find(".serce-nav-item");
@@ -46,31 +63,35 @@
             }
         };
 
+        var buildNavItems = function($img){
+            var limit;
+            mode = $img.width() / length;
+            var thumbsMarkup = '';
+            var serceNavItem = '<a class="serce-nav-item"></a>';
+            var $serceWrapper = $('<div class="serce-wrapper"></div>');
+
+            limits = [];
+            for(var i = 1; i <= length; i++){
+                limit = mode * i;
+                limits.push(limit);
+                thumbsMarkup += serceNavItem;
+            }
+
+            $wrapEl.find(".serce-wrapper").remove();
+            $serceWrapper.insertAfter($img);
+            $(thumbsMarkup).appendTo($serceWrapper);
+            $serceWrapper.find(".serce-nav-item").first().addClass("on");
+        };
+
         this.each(function() {
-            var limit,
-                $this = $(this);
+            var $this = $(this);
+            var $img = $this.find(imgSelector);
 
             $this.hover(function(){
-                var $img = $this.find(imgSelector);
                 images = $img.data("images") ? $img.data("images").split(",") : [];
                 length = images.length;
                 if(length > 1){
-                    mode = $img.width() / length;
-                    var thumbsMarkup = '';
-                    var serceNavItem = '<a class="serce-nav-item"></a>';
-                    var $serceWrapper = $('<div class="serce-wrapper"></div>');
-
-                    limits = [];
-                    for(var i = 1; i <= length; i++){
-                        limit = mode * i;
-                        limits.push(limit);
-                        thumbsMarkup += serceNavItem;
-                    }
-
-                    $this.find(".serce-wrapper").remove();
-                    $serceWrapper.insertAfter($img);
-                    $(thumbsMarkup).appendTo($serceWrapper);
-                    $serceWrapper.find(".serce-nav-item").first().addClass("on");
+                    buildNavItems($img);
                     $img.bind("mousemove", mouseMove);
                 }
             }, mouseLeave);
@@ -82,9 +103,3 @@
     };
 
 }( jQuery ));
-
-
-
-
-
-
